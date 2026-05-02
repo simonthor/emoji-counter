2935be8157802d41e627903765ed04f05ae4e895
Simon Thor <simon11.thor@hotmail.se>
2026-05-03 00:16:15 +0200

    Remove dash run call, since that is only a development server

--- index.py
+++ index.py
@@ -1,10 +1,6 @@
 from emoji_counter.explore import EmojiExplorer
 import dash
-import os
 
 app = dash.Dash(__name__)
 server = app.server
 explorer = EmojiExplorer(app)
-host = os.getenv("HOST", "0.0.0.0")
-port = int(os.getenv("PORT", 10000))
-explorer.run(host=host, port=port)
